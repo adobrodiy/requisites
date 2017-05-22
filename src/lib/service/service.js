8f2e0114315fc1602b29265c4de0abb0c0542bae
adobrodiy <adobrodiy@users.noreply.github.com>
2017-05-22 22:39:33 +0300

    service

--- src/lib/service/service.js
+++ src/lib/service/service.js
@@ -1,5 +1,10 @@
 'use strict';
-
+/**
+ * for getting first digit of control sum
+ * https://ru.wikipedia.org/wiki/%D0%98%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80_%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%89%D0%B8%D0%BA%D0%B0
+ * @param   {String}    str     string of 10 digits
+ * @returns {number}            one digit number
+ */
 function getInnFirstCheck ( str ) {
     return (
         7  * str[ 0 ] +
@@ -14,7 +19,12 @@ function getInnFirstCheck ( str ) {
         8  * str[ 9 ]
     ) % 11 % 10;
 }
-
+/**
+ * for getting second digit of control sum
+ * https://ru.wikipedia.org/wiki/%D0%98%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80_%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%89%D0%B8%D0%BA%D0%B0
+ * @param   {String}    str     string of 11 digits
+ * @returns {number}            one digit number
+ */
 function getInnSecondCheck ( str ) {
     return (
         3  * str[ 0 ] +
@@ -31,7 +41,16 @@ function getInnSecondCheck ( str ) {
     ) % 11 % 10;
 }
 
+/**
+ * @param   {Array}     array   array of any elements
+ * @returns {*}                 element of array picked randomly
+ */
+function getRandomElement ( array ) {
+    return array[ Math.floor( Math.random() * array.length ) ];
+}
+
 module.exports = {
-    getInnFirstCheck    : getInnFirstCheck,
-    getInnSecondCheck   : getInnSecondCheck
+    getInnFirstCheck    : getInnFirstCheck      ,
+    getInnSecondCheck   : getInnSecondCheck     ,
+    getRandomElement    : getRandomElement
 };
--- src/lib/service/service.test.js
+++ src/lib/service/service.test.js
@@ -5,32 +5,29 @@ const assert    = require( 'assert'         );
 
 describe( "Inn service test", function () {
 
-    describe( 'getInnFirstCheck', function () {
-
-        it( "", function () {
-            assert.equal( service.getInnFirstCheck( '5001007322' ), 5 );
-            assert.equal( service.getInnFirstCheck( '6273719830' ), 8 );
-            assert.equal( service.getInnFirstCheck( '3410548860' ), 0 );
-            assert.equal( service.getInnFirstCheck( '1554982945' ), 9 );
-            assert.equal( service.getInnFirstCheck( '5413548860' ), 0 );
-            assert.equal( service.getInnFirstCheck( '2656676186' ), 5 );
-            //assert.equal( service.getInnFirstCheck( '7728075928' ), 3 );
-        } );
-
+    it( 'getInnFirstCheck', function () {
+        assert.equal( service.getInnFirstCheck( '5001007322' ), 5 );
+        assert.equal( service.getInnFirstCheck( '6273719830' ), 8 );
+        assert.equal( service.getInnFirstCheck( '3410548860' ), 0 );
+        assert.equal( service.getInnFirstCheck( '1554982945' ), 9 );
+        assert.equal( service.getInnFirstCheck( '5413548860' ), 0 );
+        assert.equal( service.getInnFirstCheck( '2656676186' ), 5 ); // checked by hands
     } );
 
-    describe( 'getInnSecondCheck', function () {
+    it( 'getInnSecondCheck', function () {
+        assert.equal( service.getInnSecondCheck( '50010073225' ), 9 );
+        assert.equal( service.getInnSecondCheck( '62737198308' ), 5 );
+        assert.equal( service.getInnSecondCheck( '34105488600' ), 6 );
+        assert.equal( service.getInnSecondCheck( '15549829459' ), 2 );
+        assert.equal( service.getInnSecondCheck( '54135488600' ), 2 );
+        assert.equal( service.getInnSecondCheck( '26566761865' ), 2 );
+    } );
 
-        it( "", function () {
-            assert.equal( service.getInnSecondCheck( '50010073225' ), 9 );
-            //assert.equal( service.getInnSecondCheck( '77280759283' ), 6 );
-            assert.equal( service.getInnFirstCheck( '62737198308' ), 5 );
-            assert.equal( service.getInnFirstCheck( '34105488600' ), 6 );
-            assert.equal( service.getInnFirstCheck( '15549829459' ), 2 );
-            assert.equal( service.getInnFirstCheck( '54135488600' ), 2 );
-            assert.equal( service.getInnFirstCheck( '26566761865' ), 2 );
-        } );
-
+    it( 'getRandomElement', function () {
+        const array = [ 1, '2', { foo: 'bar' }, [ 42 ] ];
+        const element = service.getRandomElement( array );
+        const index = array.indexOf( element );
+        assert.notEqual( index, -1 );
     } );
 
 } );
